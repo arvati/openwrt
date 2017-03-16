fda2f08392b726a13ebacfc0dea188b50a431d30
Ademar Arvati Filho <arvati@hotmail.com>
2017-03-15 21:41:55 -0300

    several adjustments, trying get extroot back

--- files/root/autoprovision-functions.sh
+++ files/root/autoprovision-functions.sh
@@ -5,64 +5,10 @@
 # make sure that installed packages take precedence over busybox. see https://dev.openwrt.org/ticket/18523
 PATH="/usr/bin:/usr/sbin:/bin:/sbin"
 
-# these are also copy-pasted into other scripts and config files!
-rootUUID=05d615b3-bef8-460c-9a23-52db8d09e000
-dataUUID=41b8326a-1bbc-48da-91e4-448462595799
-swapUUID=05d615b3-bef8-460c-9a23-52db8d09e002
-
- # let's attempt to define some defaults...
- autoprovisionUSBLed="u35wf:red:wifi"
- autoprovisionStatusLed="u35wf:green:lan"
-
 
 log()
 {
     /usr/bin/logger -t autoprov -s $*
-}
-
-setLedAttribute()
-{
-    [ -f "/sys/class/leds/$1/$2" ] && echo "$3" > "/sys/class/leds/$1/$2"
-}
-
-signalAutoprovisionWorking()
-{
-    setLedAttribute ${autoprovisionStatusLed} trigger none
-    setLedAttribute ${autoprovisionStatusLed} trigger timer
-    setLedAttribute ${autoprovisionStatusLed} delay_on 2000
-    setLedAttribute ${autoprovisionStatusLed} delay_off 2000
-}
-
-signalAutoprovisionWaitingForUser()
-{
-    setLedAttribute ${autoprovisionStatusLed} trigger none
-    setLedAttribute ${autoprovisionStatusLed} trigger timer
-    setLedAttribute ${autoprovisionStatusLed} delay_on 200
-    setLedAttribute ${autoprovisionStatusLed} delay_off 300
-}
-
-signalWaitingForPendrive()
-{
-    setLedAttribute ${autoprovisionUSBLed} trigger none
-    setLedAttribute ${autoprovisionUSBLed} trigger timer
-    setLedAttribute ${autoprovisionUSBLed} delay_on 200
-    setLedAttribute ${autoprovisionUSBLed} delay_off 300
-}
-
-signalFormatting()
-{
-    setLedAttribute ${autoprovisionUSBLed} trigger none
-    setLedAttribute ${autoprovisionUSBLed} trigger timer
-    setLedAttribute ${autoprovisionUSBLed} delay_on 1000
-    setLedAttribute ${autoprovisionUSBLed} delay_off 1000
-}
-
-stopSignallingAnything()
-{
-    # TODO this is wrong, they should be restored to their original state.
-    # but then leds are only touched in the setup stage, which is ephemeral when things work as expected...
-    setLedAttribute ${autoprovisionStatusLed} trigger none
-    setLedAttribute ${autoprovisionUSBLed} trigger usbdev
 }
 
 setRootPassword()
@@ -76,3 +22,119 @@ setRootPassword()
     log "Setting root password"
     echo -e "$password\n$password\n" | passwd root
 }
+
+step() {
+    echo -n "$@"
+    STEP_OK=0
+    [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+}
+
+try() {
+    # Check for `-b' argument to run command in the background.
+    local BG=
+    [[ $1 == -b ]] && { BG=1; shift; }
+    [[ $1 == -- ]] && {       shift; }
+    # Run the command.
+    if [[ -z $BG ]]; then
+        "$@"
+    else
+        "$@" &
+    fi
+    # Check if command failed and update $STEP_OK if so.
+    local EXIT_CODE=$?
+    if [[ $EXIT_CODE -ne 0 ]]; then
+        STEP_OK=$EXIT_CODE
+        [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+    fi
+    return $EXIT_CODE
+}
+
+next() {
+    [[ -f /tmp/step.$$ ]] && { STEP_OK=$(< /tmp/step.$$); rm -f /tmp/step.$$; }
+    [[ $STEP_OK -eq 0 ]]  && echo_success || echo_failure
+    echo
+    return $STEP_OK
+}
+
+getPendriveSize()
+{
+    # this is needed for the mmc card in some (all?) Huawei 3G dongle.
+    # details: https://dev.openwrt.org/ticket/10716#comment:4
+    if [ -e /dev/sdb ]; then
+        # force re-read of the partition table
+        head -c 1024 /dev/sdb >/dev/null
+    fi
+
+    if (grep -q sdb /proc/partitions) then
+        cat /sys/block/sdb/size
+    else
+        echo 0
+    fi
+}
+
+setupExtroot()
+{
+	local type=$1
+	local device=$2
+	#dd if=/dev/zero of="${device}" bs=1M count=1
+	wipefs --all "${device}"
+	mkfs.btrfs -f -M -d dup -m dup -L extroot "${device}"
+	log "Finished setting up filesystem"
+	mkdir -p /mnt/extroot/
+        #mount -t btrfs LABEL=extroot /mnt/extroot
+        mount -t btrfs "${device}" /mnt/extroot
+	if [ "${type}" == "overlay" ]; then
+		btrfs subvolume create /mnt/extroot/overlay
+		mkdir -p /mnt/extroot/overlay/upper/
+		btrfs subvolume create /mnt/extroot/overlay/upper/home
+		btrfs subvolume create /mnt/extroot/rootfs/upper/srv		
+		mkdir -p /mnt/extroot/overlay/upper/etc/
+    		cat >/mnt/extroot/overlay/upper/etc/rc.local <<EOF
+/root/autoprovision-stage2.sh
+exit 0
+EOF
+		uci get fstab.overlay && uci delete fstab.overlay
+		uci set fstab.overlay=mount
+		uci set fstab.overlay.target='/overlay'
+		uci set fstab.overlay.fstype='btrfs'
+		uci set fstab.overlay.device="${device}"
+		local subvolid=$(btrfs subvolume list -t /mnt/extroot | grep overlay | cut -c1-3)
+		uci set fstab.overlay.options='subvolid=/overlay'
+		uci set fstab.overlay.enabled='1'
+		uci get fstab.rootfs && uci set fstab.rootfs.enabled='0'
+    	else
+		btrfs subvolume create /mnt/extroot/rootfs
+		btrfs subvolume create /mnt/extroot/rootfs/home
+		btrfs subvolume create /mnt/extroot/rootfs/srv
+		mkdir -p /tmp/introot
+		mount --bind / /tmp/introot
+		tar -C /tmp/introot -cvf - . | tar -C /mnt/extroot/rootfs -xf -
+		umount /tmp/introot
+		#rsync -avxH / /mnt/extroot/rootfs/
+		mkdir -p /mnt/extroot/rootfs/etc/
+    		cat >/mnt/extroot/rootfs/etc/rc.local <<EOF
+/root/autoprovision-stage2.sh
+exit 0
+EOF
+		uci get fstab.rootfs && uci delete fstab.rootfs
+		uci set fstab.rootfs=mount
+		uci set fstab.rootfs.target='/'
+		uci set fstab.rootfs.fstype='btrfs'
+		uci set fstab.rootfs.device="${device}"
+		local subvolid=$(btrfs subvolume list -t /mnt/extroot | grep rootfs | cut -c1-3)
+		uci set fstab.rootfs.options='subvolid=/rootfs'
+		uci set fstab.rootfs.enabled='1'
+		uci get fstab.overlay && uci set fstab.overlay.enabled='0'
+    	fi
+    	uci set fstab.@global[0].delay_root='15'
+	uci set fstab.@global[0].anon_swap='0'
+	uci set fstab.@global[0].anon_mount='0'
+	uci set fstab.@global[0].auto_swap='0'
+	uci set fstab.@global[0].auto_mount='0'
+	uci set fstab.@global[0].check_fs='0'
+    	#uci show fstab	
+    	uci commit fstab 
+	umount /mnt/extroot
+	log "Finished setting up extroot"
+}
+
--- files/root/autoprovision-stage1.sh
+++ files/root/autoprovision-stage1.sh
@@ -4,134 +4,28 @@
 
 . /root/autoprovision-functions.sh
 
-getPendriveSize()
-{
-    # this is needed for the mmc card in some (all?) Huawei 3G dongle.
-    # details: https://dev.openwrt.org/ticket/10716#comment:4
-    if [ -e /dev/sda ]; then
-        # force re-read of the partition table
-        head -c 1024 /dev/sda >/dev/null
-    fi
-
-    if (grep -q sda /proc/partitions) then
-        cat /sys/block/sda/size
-    else
-        echo 0
-    fi
-}
-
-hasBigEnoughPendrive()
-{
-    local size=$(getPendriveSize)
-    if [ $size -ge 600000 ]; then
-        log "Found a pendrive of size: $(($size / 2 / 1024)) MB"
-        return 0
-    else
-        return 1
-    fi
-}
-
-setupPendrivePartitions()
-{
-    # erase partition table
-    dd if=/dev/zero of=/dev/sda bs=1M count=1
-
-    # sda1 is 'swap'
-    # sda2 is 'root'
-    # sda3 is 'data'
-    fdisk /dev/sda <<EOF
-o
-n
-p
-1
-
-+64M
-n
-p
-2
-
-+4096M
-n
-p
-3
-
-
-t
-1
-82
-w
-EOF
-    log "Finished partitioning /dev/sda using fdisk"
-
-    sleep 2
-
-    until [ -e /dev/sda1 ]
-    do
-        echo "Waiting for partitions to show up in /dev"
-        sleep 1
-    done
-
-    mkswap -L swap -U $swapUUID /dev/sda1
-    mkfs.ext4 -L root -U $rootUUID /dev/sda2
-    mkfs.btrfs -L data -U $dataUUID /dev/sda3
-
-    log "Finished setting up filesystems"
-}
-
-setupExtroot()
-{
-    mkdir -p /mnt/extroot/
-    mount -U $rootUUID /mnt/extroot
-
-    overlay_root=/mnt/extroot/upper
-
-    # at this point we could copy the entire root (a previous version of this script did that), or just the overlay from the flash,
-    # but it seems to work fine if we just create an empty overlay that is only replacing the rc.local from the firmware.
-
-    # let's write a new rc.local on the extroot that will shadow the one which is in the rom (to run stage2 instead of stage1)
-    mkdir -p ${overlay_root}/etc/
-    cat >${overlay_root}/etc/rc.local <<EOF
-/root/autoprovision-stage2.sh & 
-exit 0
-EOF
-
-    # TODO FIXME when this below is enabled then Chaos Calmer doesn't turn on the network and the device remains unreachable
-
-    # make sure that we shadow the /var -> /tmp symlink in the new extroot, so that /var becomes persistent across reboots.
-#    mkdir -p ${overlay_root}/var
-    # KLUDGE: /var/state is assumed to be transient, so link it to tmp, see https://dev.openwrt.org/ticket/12228
-#    cd ${overlay_root}/var
-#    ln -s /tmp state
-#    cd -
-
-    log "Finished setting up extroot"
-}
-
 autoprovisionStage1()
 {
-#    signalAutoprovisionWorking
-
-#    signalAutoprovisionWaitingForUser
-#    signalWaitingForPendrive
-
-    until hasBigEnoughPendrive
-    do
-        echo "Waiting for a pendrive to be inserted"
-        sleep 3
-    done
-
-#    signalAutoprovisionWorking # to make it flash in sync with the USB led
-#    signalFormatting
-
-#    sleep 1
-#    setupPendrivePartitions
-
-    sleep 1
-    setupExtroot
-
-    sync
-#    stopSignallingAnything
-#    reboot
+	log "Autoprovisioning stage1 speaking"
+	local device="/dev/sda"
+	#local type="overlay"
+	local type="rootfs"
+	local FOUND=1
+	btrfs fi show -d "${device}" || FOUND=0
+	if [ $FOUND == 0 ]; then
+		setupExtroot "${type}" "${device}"
+		sync
+        	reboot
+        else
+        	log "Found BTRFS disk nothing done"
+        	uci set fstab.@global[0].delay_root='15'
+		uci set fstab.@global[0].anon_swap='0'
+		uci set fstab.@global[0].anon_mount='0'
+		uci set fstab.@global[0].auto_swap='0'
+		uci set fstab.@global[0].auto_mount='0'
+		uci set fstab.@global[0].check_fs='0'
+        	uci commit fstab
+        fi
 }
 
 autoprovisionStage1
